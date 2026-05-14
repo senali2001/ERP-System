107340ede2975a23076d24afe69c2dcdf0061d69
senali2001 <rbnsenali@students.nsbm.ac.lk>
2026-05-14 21:49:57 +0530

    feat: added docker setup and backend test configurations
    so far this create docker images erp backend and postgres

---
+++ backend/src/test/java/com/erp/backend/simpletest.java
@@ -0,0 +1,13 @@
+package com.erp.backend;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class simpletest {
+
+    @Test
+    void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
